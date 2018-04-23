42138e06c2b993547f5e3e31b9f0870780fbc8e2
root <ashutoshsinghtomar.ashu@gmail.com>
2018-04-23 16:56:09 +0530

    Enormous Input Test | Problem Code:INTEST

---
+++ CodeChef/Beginner/Enormous Input Test Problem Code: INTEST.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Demo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,k,t,output=0;
+		Scanner s= new Scanner(System.in);
+		n=s.nextInt();
+		k=s.nextInt();
+
+		for(int I=0;I<n;I ++){
+		t=s.nextInt();
+		if(t%k==0)
+		output+=1;
+
+			}
+			System.out.println(output);
+
+	}
+}
