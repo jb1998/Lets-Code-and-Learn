a03cb76138d39aa2c8c9897e0714930477a79153
root <ashutoshsinghtomar.ashu@gmail.com>
2018-04-23 16:53:57 +0530

    ATM | Problem Code:HS08TEST

---
+++ CodeChef/Beginner/ATM Problem Code: HS08TEST.java
@@ -0,0 +1,22 @@
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+
+     
+    class Demo
+    {
+    	public static void main (String[] args) throws java.lang.Exception
+    	{
+    		int withd=0;double initial=0.0;
+    		Scanner s= new Scanner(System.in);
+    		withd=s.nextInt();
+    		initial=s.nextDouble();
+
+    		if ((withd+0.50)>initial)
+    		System.out.println(initial);
+    		else if (withd%5==0)
+    			System.out.println(""+(initial-withd-0.50));
+    			else
+    		System.out.println(initial);
+    	}
+    }
