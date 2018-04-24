b4a5e52210d80095debdb834e3dc73dfad5128db
root <ashutoshsinghtomar.ashu@gmail.com>
2018-04-24 15:53:31 +0530

    Find Remainder | Problem Code: FLOW002

---
+++ CodeChef/Beginner/Find Remainder | Problem Code: FLOW002.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Demo
+{
+  public static void main (String[] args)
+  {
+    int t=0;
+    Scanner s= new Scanner(System.in);
+    t=s.nextInt();
+
+  for (int i=0;i<t;i++ ) {
+    int a=s.nextInt();
+    int b=s.nextInt();
+    System.out.println(a%b);
+    }
+  }
+}
