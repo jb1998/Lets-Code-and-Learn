e2581698fab8f3443b4d559cdd02d731a100cbb3
207ashu <207ashu@gmail.com>
2018-07-28 19:31:35 +0530

    If Else

---
+++ Learning Python/if_else.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+if (N%2)==0:
+    if N>=2 and N<=5:
+        print("Not Weird")
+    elif N>=6 and N<=20:
+        print("Weird")
+    elif N>20:
+        print("Not Weird")
+else:
+    print("Weird")
+
